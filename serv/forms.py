97f34365b7931e4a70836153667f177512012dcb
EDWARDINIO <37109535+EDWARDINIO@users.noreply.github.com>
2020-08-07 13:54:20 +0100

    Added a password strength valdiator

--- serv/forms.py
+++ serv/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, SubmitField, SelectField, PasswordField
 from wtforms.validators import DataRequired, Length, Email, ValidationError
 from serv import db, models
 from serv.models import User
+import re as regex
 
 
 class UploadFile(FlaskForm):
@@ -39,6 +40,31 @@ class RegisterForm(FlaskForm):
         user = User.query.filter_by(username=email.data).first()
         if user:
             raise ValidationError('Email already taken')
+
+    def validate_password(form, field):
+        """
+        Validates the given password against a predefined list of rules
+        as defined by the specification
+        :param form: Form object to validate
+        :param field: Specific field to validate
+        :return: None or raises a ValidationError
+        """
+        password = field.data
+        for x in range(len(password)):
+            if len(password) < 8:
+                raise ValidationError('Password not long enough')
+            if len(password) > 64:
+                raise ValidationError('Password too long')
+            elif not regex.search("[a-z]", password):
+                raise ValidationError('Password does not contain any lowercase characters')
+            elif not regex.search("[A-Z]", password):
+                raise ValidationError('Password does not contain any uppercase characters')
+            elif not regex.search("[0-9]", password):
+                raise ValidationError('Password does not contain any numbers')
+            elif not regex.search("[{(_@$%&~#£*^?!)}]", password):
+                raise ValidationError('Password does not contain any (valid) symbols')
+            elif regex.search("\s", password):
+                raise ValidationError('Password contains whitespace')
 
 
 class LoginForm(FlaskForm):
